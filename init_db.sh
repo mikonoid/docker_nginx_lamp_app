8d0330c911d9115e4d245195406e476b09a40c19
Mike Ivanov <mikonoid@gmail.com>
2017-03-06 10:28:16 +0200

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-
-
 /usr/bin/mysqld_safe &
 sleep 5
 mysql -u root -e "CREATE DATABASE test"
 mysql -u root test < test.sql
 
-
-
-
+#create user for test DB
 PASS=123456
-echo "=> Creating MySQL test user with ${_word} password"
-
 mysql -uroot -e "CREATE USER 'test'@'%' IDENTIFIED BY '$PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' WITH GRANT OPTION"
