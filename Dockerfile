225412602b3660c7aa7837af377b77cd1f696479
root <root@dockerhost>
2017-03-06 08:22:05 +0000

    firesst commit

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM ubuntu:trusty
+MAINTAINER Mike Ivanov
+#nginx_reverse_proxy+lamp stack+php_app
+
+# Install all packages
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update && \
+  apt-get -y install nginx supervisor git apache2 libapache2-mod-php5 mysql-server php5-mysql pwgen php-apc php5-mcrypt && \
+  echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+#Add configs and scripts
+ADD start-apache2.sh /start-apache2.sh
+ADD start-mysqld.sh /start-mysqld.sh
+ADD start-nginx.sh /start-nginx.sh
+ADD start.sh /start.sh
+RUN chmod 755 /*.sh
+ADD my.cnf /etc/mysql/conf.d/my.cnf
+ADD supervisord-apache2.conf /etc/supervisor/conf.d/supervisord-apache2.conf
+ADD supervisord-mysqld.conf /etc/supervisor/conf.d/supervisord-mysqld.conf
+ADD supervisord-nginx.conf /etc/supervisor/conf.d/supervisord-nginx.conf
+
+# Remove pre-installed database directory
+RUN rm -rf /var/lib/mysql/*
+
+# Add MySQL scripts 
+ADD test.sql /test.sql
+ADD init_db.sh /init_db.sh
+RUN chmod 755 /*.sh
+
+# apache config
+ADD apache_default /etc/apache2/sites-available/000-default.conf
+ADD apache_ports /etc/apache2/ports.conf
+RUN a2enmod rewrite
+
+#nginx config
+ADD nginx_default /etc/nginx/sites-enabled/default
+
+# Configure /www folder with php-application
+RUN git clone https://github.com/mikonoid/-php_app_for_docker /www
+RUN mkdir -p /www && rm -fr /var/www/html && ln -s /www /var/www/html
+
+# Add volumes for MySQL 
+VOLUME  ["/etc/mysql", "/var/lib/mysql" ]
+
+EXPOSE 80 3306
+CMD ["/start.sh"]
---
+++ test.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 5.5.54, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.5.54-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `record`
+--
+
+DROP TABLE IF EXISTS `record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `record` (
+  `record_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `record_name` varchar(30) NOT NULL,
+  `record_value` text NOT NULL,
+  PRIMARY KEY (`record_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `record`
+--
+
+LOCK TABLES `record` WRITE;
+/*!40000 ALTER TABLE `record` DISABLE KEYS */;
+INSERT INTO `record` VALUES (1,'xzxczxc',''),(2,'xzxczxc',''),(3,'xzxczxc',''),(4,'xzxczxc',''),(5,'123','upup'),(6,'xzxczxc',''),(7,'ggggggg',''),(8,'ggggggg',''),(9,'ggggggg444',''),(10,'sdfsdf','44444444'),(11,'sdfsdf','44444444');
+/*!40000 ALTER TABLE `record` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-05 16:00:23
